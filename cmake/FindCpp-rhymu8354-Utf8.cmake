f8beef19f555bf7bd4edcd963bb2791389561730
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-03-23 16:29:02 +0000

    Updated Utf8

--- cmake/FindCpp-rhymu8354-Utf8.cmake
+++ cmake/FindCpp-rhymu8354-Utf8.cmake
@@ -1,5 +1,6 @@
 CPMAddPackage(
     NAME            Cpp-rhymu8354-Utf8
     GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-rhymu8354-Utf8
-    GIT_TAG         bd833c42b3c2b2e6f25891ab898c46ddd6ae97ed
+    GIT_BRANCH      Updated
+    GIT_TAG         9977512e500a20eb579f27154399231696215fc8
 )
