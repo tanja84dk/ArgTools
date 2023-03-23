4e1059a4aa4865378ac35b0f022ee97841140615
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-03-23 16:29:17 +0000

    Updated Base64

--- cmake/FindCpp-rhymu8354-Base64.cmake
+++ cmake/FindCpp-rhymu8354-Base64.cmake
@@ -1,5 +1,6 @@
 CPMAddPackage(
     NAME            Cpp-rhymu8354-Base64
     GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-rhymu8354-Base64
-    GIT_TAG         updated
+    GIT_BRANCH      Updated
+    GIT_TAG         0d2bdad7d480dc3eec358836a824c3f774eeacfe
 )
