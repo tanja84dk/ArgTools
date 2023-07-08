26a158acad3044be24faa0bbdb9471bf6e0a4e96
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-08 19:30:36 +0000

    fixed license header

--- src/licenses.h
+++ src/licenses.h
@@ -1,43 +1,50 @@
 #ifndef TANJA84DK_ARGTOOLS_LICENSES_H
 #define TANJA84DK_ARGTOOLS_LICENSES_H
 #include <ArgTools_licenses_macro.h>
+#include <Tanja84dk/crypt/licenses.h>
 #include <string>
-
-namespace
-{
-    void print_title_for_licenses(const std::string &title)
-    {
-        printf("---------------------------\n"
-               "    %s\n"
-               "---------------------------\n",
-               title.c_str());
-    }
-
-    void replace_all(std::string &str, const std::string &from, const std::string &to)
-    {
-        if (from.empty())
-            return;
-        size_t start_pos = 0;
-        while ((start_pos = str.find(from, start_pos)) != std::string::npos)
-        {
-            str.replace(start_pos, from.length(), to);
-            start_pos += to.length();
-        }
-    }
-}
 
 namespace Tanja84dk::argtools::license
 {
+    namespace
+    {
+        void print_title_for_licenses(const std::string &title)
+        {
+            printf("---------------------------\n"
+                   "    %s\n"
+                   "---------------------------\n",
+                   title.c_str());
+        }
+
+        void replace_all(std::string &str, const std::string &from, const std::string &to)
+        {
+            if (from.empty())
+                return;
+            size_t start_pos = 0;
+            while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+            {
+                str.replace(start_pos, from.length(), to);
+                start_pos += to.length();
+            }
+        }
+    }
     std::string get_argtools_license()
     {
-        std::string license = ArgTools_LICENSE;
+        std::string license = tanja84dk_argtools_LICENSE;
         replace_all(license, ";", "\n");
         return license;
     };
 
+    std::string get_tanja84dk_crypt_license()
+    {
+        std::string license = tanja84dk_crypt_LICENSE;
+        replace_all(license, ";", "\n");
+        return license;
+    }
+
     std::string get_tanja84dk_tools_license()
     {
-        std::string license = tanja84dk_tools_LICENSE;
+        std::string license = Tanja84dk_tools_LICENSE;
         replace_all(license, ";", "\n");
         return license;
     };
@@ -60,6 +67,9 @@ namespace Tanja84dk::argtools::license
     {
         print_title_for_licenses("ArgTools");
         printf("%s\n\n", get_argtools_license().c_str());
+
+        print_title_for_licenses("tanja84dk_crypt");
+        printf("%s\n\n", get_tanja84dk_crypt_license().c_str());
 
         print_title_for_licenses("tanja84dk_tools");
         printf("%s\n\n", get_tanja84dk_tools_license().c_str());
