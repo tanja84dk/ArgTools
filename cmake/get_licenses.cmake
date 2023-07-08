3dd9d7505631ac540ecae1653b7e4ec4193dbef6
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-08 19:02:35 +0000

    cleanup

--- cmake/get_licenses.cmake
+++ cmake/get_licenses.cmake
@@ -1,15 +1,32 @@
-file(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE ArgTools_LICENSE)
-string(PREPEND ArgTools_LICENSE "R\"(")
-string(APPEND ArgTools_LICENSE ")\"")
 
-file(STRINGS ${Tanja84dkTools_SOURCE_DIR}/LICENSE tanja84dk_tools_LICENSE)
-string(PREPEND tanja84dk_tools_LICENSE "R\"(")
-string(APPEND tanja84dk_tools_LICENSE ")\"")
+set(${PROJECT_NAME}_LICENSES_MACRO_FILE ${${PROJECT_NAME}_BINARY_DIR}/${PROJECT_NAME}_licenses_macro.h.in)
 
-file(STRINGS ${cryptopp-cmake_SOURCE_DIR}/LICENSE cryptopp_cmake_LICENSE)
-string(PREPEND cryptopp_cmake_LICENSE "R\"(")
-string(APPEND cryptopp_cmake_LICENSE ")\"")
+if(EXISTS "${${PROJECT_NAME}_LICENSES_MACRO_FILE}")
+    file(WRITE "${${PROJECT_NAME}_LICENSES_MACRO_FILE}" "")
+endif()
 
-file(STRINGS ${cryptopp-cmake_BINARY_DIR}/cryptopp/License.txt cryptopp_LICENSE)
-string(PREPEND cryptopp_LICENSE "R\"(")
-string(APPEND cryptopp_LICENSE ")\"")
+file(APPEND "${${PROJECT_NAME}_LICENSES_MACRO_FILE}" "#ifndef TANJA84DK_ARGTOOLS_LICENCES_MACRO_H\n")
+file(APPEND "${${PROJECT_NAME}_LICENSES_MACRO_FILE}" "#define TANJA84DK_ARGTOOLS_LICENCES_MACRO_H\n\n")
+
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+    file(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE tanja84dk_argtools_LICENSE)
+    string(PREPEND tanja84dk_argtools_LICENSE "R\"(")
+    string(APPEND tanja84dk_argtools_LICENSE ")\"")
+    file(APPEND ${${PROJECT_NAME}_LICENSES_MACRO_FILE} "#define tanja84dk_argtools_LICENSE @tanja84dk_argtools_LICENSE@\n")
+else()
+    message("tanja84dk_argtools_LICENSE is missing")
+endif()
+
+if(EXISTS "${Tanja84dk_tools_SOURCE_DIR}/LICENSE")
+    file(STRINGS ${Tanja84dk_tools_SOURCE_DIR}/LICENSE Tanja84dk_tools_LICENSE)
+    string(PREPEND Tanja84dk_tools_LICENSE "R\"(")
+    string(APPEND Tanja84dk_tools_LICENSE ")\"")
+    file(APPEND ${${PROJECT_NAME}_LICENSES_MACRO_FILE} "#define Tanja84dk_tools_LICENSE @Tanja84dk_tools_LICENSE@\n")
+else()
+    message("Tanja84dk_tools_LICENSE is missing")
+endif()
+
+file(APPEND "${${PROJECT_NAME}_LICENSES_MACRO_FILE}"
+"\n#endif // TANJA84DK_ARGTOOLS_LICENCES_MACRO_H\n")
+
+configure_file(${${PROJECT_NAME}_LICENSES_MACRO_FILE} include/${PROJECT_NAME}_licenses_macro.h)
