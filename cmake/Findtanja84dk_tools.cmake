c332cc3471515970e1bf2783f64a97361327115a
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-10 20:15:21 +0000

    renamed and updated tanja84dk_tools to my convention

--- cmake/Findtanja84dk_tools.cmake
+++ cmake/Findtanja84dk_tools.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
-    NAME            Tanja84dkTools
+    NAME            tanja84dk_tools
     GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-Tanja84dk-Tools
     VERSION         0.1.1
-    GIT_TAG         83930f484458926e962d3ed67da4d5d1e85b8c9c
+    GIT_TAG         ad7fab2aae43e1e80a064a3d94ad49027e342b50
 )
