a9291580fda52f44164bed4ee5207645df51229d
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-14 12:59:27 +0000

    Updated library tanja84dk_tools

--- cmake/Findtanja84dk_tools.cmake
+++ cmake/Findtanja84dk_tools.cmake
@@ -2,5 +2,5 @@ CPMAddPackage(
     NAME            tanja84dk_tools
     GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-Tanja84dk-Tools
     VERSION         0.1.1
-    GIT_TAG         ad7fab2aae43e1e80a064a3d94ad49027e342b50
+    GIT_TAG         f0707e919ca7aadb775b8e95ecbf226c0642435f
 )
