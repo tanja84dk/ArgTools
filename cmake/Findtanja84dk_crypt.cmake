c45f58b5dfa3ff96a31fa6b774bd5152f65b779d
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-15 21:51:49 +0000

    Updated version

--- cmake/Findtanja84dk_crypt.cmake
+++ cmake/Findtanja84dk_crypt.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
     NAME            tanja84dk_crypt
     GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-Tanja84dk-Crypt
-    VERSION         0.0.1
-    GIT_TAG         4d15110b795f040d580d98b7642e298e369dae6d
+    VERSION         0.1.0
+    GIT_TAG         bb50633bb4c09ef1769765f46a7fac59b127c130
 )
